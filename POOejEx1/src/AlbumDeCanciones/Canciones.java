7234e466d391b254ee0c3476987742794a49c4c5
Elio Chamorro <elio19.1991@gmail.com>
2023-04-03 19:52:05 -0300

    Agregado el ejercicio Extra N°1

---
+++ POOejEx1/src/AlbumDeCanciones/Canciones.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AlbumDeCanciones;
+
+import Entidades.Cancion;
+
+/**
+ *
+ * @author Elio
+ */
+public class Canciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Cancion can = new Cancion();
+       
+       can.Canciones();
+       can.MostrarCancion();
+        
+    }
+    
+}
