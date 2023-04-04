6d2807abefeae52d6ecc21109d33d4a57303e194
Elio Chamorro <elio19.1991@gmail.com>
2023-04-03 23:53:20 -0300

    Agregue el ejercicio N°4

--- POOejEx1/src/AlbumDeCanciones/Canciones.java
+++ POOejEx1/src/AlbumDeCanciones/Canciones.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Desarrollar una clase Cancion con los siguientes atributos: título y autor. 
+Se deberá definir además dos constructores:
+uno vacío que inicializa el título y el autor a cadenas vacías 
+y otro que reciba como parámetros el título y el autor de la canción. 
+Se deberán además definir los métodos getters y setters correspondientes.
  */
 package AlbumDeCanciones;
 
