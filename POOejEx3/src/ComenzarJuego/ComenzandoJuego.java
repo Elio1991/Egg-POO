2a9a2070260482b0d14f8502221da798c42eaaaa
Elio Chamorro <elio19.1991@gmail.com>
2023-04-03 23:11:47 -0300

    Agregue el ejercicio N°3

---
+++ POOejEx3/src/ComenzarJuego/ComenzandoJuego.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ComenzarJuego;
+
+import Entidades.Juego;
+
+/**
+ *
+ * @author Elio
+ */
+public class ComenzandoJuego {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Juego jugar = new Juego();
+        
+        jugar.Iniciar_Juego();
+        
+        
+        
+    }
+    
+}
