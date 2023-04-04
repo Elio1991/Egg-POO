dfea4c9d7cb2d0253769357bb13edbdd772c535e
Elio Chamorro <elio19.1991@gmail.com>
2023-04-04 00:31:23 -0300

    Agregue el ejercicio N°5

---
+++ POOejEx5/src/Empleados/SalarioDeEmpleados.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Empleados;
+
+import Entidades.Empleado;
+
+/**
+ *
+ * @author Elio
+ */
+public class SalarioDeEmpleados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Empleado emp1 = new Empleado();
+        
+        emp1.Calcular_aumento();
+        
+        
+    }
+    
+}
