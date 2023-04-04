9db34028f2d4c85e644d2f1f1081a441c8cd77a0
Elio Chamorro <elio19.1991@gmail.com>
2023-04-04 01:12:11 -0300

    Agregue el ejercicio Extra 6

--- POOejEx6/src/Entidades/Rectangulo.java
+++ POOejEx6/src/Entidades/Rectangulo.java
@@ -1,5 +1,5 @@
 /*
-Crea una clase "Rectángulo" que tenga atributos "lado1" y "lado2"
+Crea una clase "Rectángulo" que tenga atributos "lado1" y "lado2" 
 y un método "calcular_area" que calcule y devuelva el área del rectángulo.
 Luego crea un objeto "rectangulo1" de la clase "Rectángulo" con lados de 4 y 6
 y imprime el área del rectángulo.
