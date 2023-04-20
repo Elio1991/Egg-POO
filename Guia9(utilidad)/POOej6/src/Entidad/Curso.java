772b8b568cf7d6014b7d35373f722254eb22a6db
Elio Chamorro <elio19.1991@gmail.com>
2023-04-20 20:47:37 -0300

    Ejercicio N°6

---
+++ Guia9(utilidad)/POOej6/src/Entidad/Curso.java
@@ -0,0 +1,101 @@
+/*
+Uso de vectores como atributos de clase
+Un profesor particular está dando cursos para grupos de cinco alumnos y necesita un programa
+para organizar la información de cada curso. 
+Para ello, crearemos una clase entidad llamada Curso, cuyos atributos serán: 
+nombreCurso, cantidadHorasPorDia, cantidadDiasPorSemana, turno (mañana o tarde),  precioPorHora y alumnos.
+Donde alumnos es un arreglo de tipo String de dimensión 5 (cinco), donde se alojarán los nombres de cada alumno.
+A continuación, se implementarán los siguientes métodos:
+Un constructor por defecto.
+Un constructor con todos los atributos como parámetro.
+Métodos getters y setters de cada atributo.
+Método cargarAlumnos(): este método le permitirá al usuario ingresar los alumnos que asisten a las clases.
+Nosotros nos encargaremos de almacenar esta información en un arreglo e iterar con un bucle, 
+solicitando en cada repetición que se ingrese el nombre de cada alumno.
+Método crearCurso(): el método crear curso, le pide los valores de los atributos al usuario
+y después se le asignan a sus respectivos atributos para llenar el objeto Curso.
+En este método invocamos al método cargarAlumnos() para asignarle valor al atributo alumnos
+Método calcularGananciaSemanal(): este método se encarga de calcular la ganancia en una semana por curso.
+Para ello, se deben multiplicar la cantidad de horas por día, el precio por hora, 
+la cantidad de alumnos y la cantidad de días a la semana que se repite el encuentro.
+
+ */
+package Entidad;
+
+/**
+ *
+ * @author Elio
+ */
+public class Curso {
+    
+    private String nombreCurso;
+    private int cantidadHorasPorDia;
+    private int cantidadDiasPorSemana;
+    private String turnoMañanaOTarde;
+    private float precioPorHora;
+    private String[] alumnos;
+
+    public Curso() {
+    }
+
+    public Curso(String nombreCurso, int cantidadHorasPorDia, int cantidadDiasPorSemana, String turnoMañanaOTarde, float precioPorHora, String[] alumnos) {
+        this.nombreCurso = nombreCurso;
+        this.cantidadHorasPorDia = cantidadHorasPorDia;
+        this.cantidadDiasPorSemana = cantidadDiasPorSemana;
+        this.turnoMañanaOTarde = turnoMañanaOTarde;
+        this.precioPorHora = precioPorHora;
+        this.alumnos = alumnos;
+    }
+
+    public String getNombreCurso() {
+        return nombreCurso;
+    }
+
+    public void setNombreCurso(String nombreCurso) {
+        this.nombreCurso = nombreCurso;
+    }
+
+    public int getCantidadHorasPorDia() {
+        return cantidadHorasPorDia;
+    }
+
+    public void setCantidadHorasPorDia(int cantidadHorasPorDia) {
+        this.cantidadHorasPorDia = cantidadHorasPorDia;
+    }
+
+    public int getCantidadDiasPorSemana() {
+        return cantidadDiasPorSemana;
+    }
+
+    public void setCantidadDiasPorSemana(int cantidadDiasPorSemana) {
+        this.cantidadDiasPorSemana = cantidadDiasPorSemana;
+    }
+
+    public String getTurnoMañanaOTarde() {
+        return turnoMañanaOTarde;
+    }
+
+    public void setTurnoMañanaOTarde(String turnoMañanaOTarde) {
+        this.turnoMañanaOTarde = turnoMañanaOTarde;
+    }
+
+    public float getPrecioPorHora() {
+        return precioPorHora;
+    }
+
+    public void setPrecioPorHora(float precioPorHora) {
+        this.precioPorHora = precioPorHora;
+    }
+
+    public String[] getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(String[] alumnos) {
+        this.alumnos = alumnos;
+    }
+    
+    
+    
+    
+}
